22d337d0e822033b67204e1b955e32a8644fca1c
sujeonge <positivemyself23@gmail.com>
2024-02-05 14:13:37 +0900

    section4

--- assets/js/script (2).js
+++ assets/js/script (2).js
@@ -35,4 +35,56 @@ $(document).ready(function () {
             prevEl: '.swiper-button-prev',
         },
     });
+
+    // 섹션03 갤러리 썸네일
+    var thumbSwiper = new Swiper('.gallery_thumb', {
+        spaceBetween: 0,
+        loop: true,
+        touchRatio: 0,
+        breakpoints: {
+            768: {
+                direction: 'vertical',
+            },
+        },
+    });
+    // 섹션03 갤러리 메인
+    var mainSwiper = new Swiper('.gallery_main', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        loop: true,
+        thumbs: {
+            swiper: thumbSwiper,
+        },
+    });
+
+        // 섹션05 커머스 글귀
+        var videoSwiper = new Swiper('.commerce_wrap', {
+            slidesPerView: 'auto',
+            spaceBetween: 16,
+            loop: true,
+            loopedSlides: 3,
+            slideToClickedSlide: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            on: {
+                slideChangeTransitionEnd: function () {
+                    $('.progress-bar').removeClass('active');
+                    $('.swiper-slide-active .progress .progress-bar').addClass('active');
+                },
+            },
+        });
+    
+        // 섹션05 커머스 비디오
+        var videoMainSwiper = new Swiper('.commerce_video', {
+            slidesPerView: 1,
+            loop: true,
+            loopedSlides: 3,
+        });
+    
+        videoMainSwiper.controller.control = videoSwiper;
+        videoSwiper.controller.control = videoMainSwiper;
 });
